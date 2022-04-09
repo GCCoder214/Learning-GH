8ffa3856992852116b1440c7910b244ae4a63ad0
Gabriel <gabrielcarreiro@iCloud.com>
2022-04-09 18:24:08 +0900

    Initial Commit

---
+++ Lesson_2/Lesson_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Lesson_2App.swift
+//  Lesson_2
+//
+//  Created by Gabriel Carreiro on 4/9/22.
+//
+
+import SwiftUI
+
+@main
+struct Lesson_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
